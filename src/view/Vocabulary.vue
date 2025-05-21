317aa67762b0d55671ad2464e6905109749bbef4
KyleBing <kylebing@163.com>
2025-05-21 14:14:03 +0800

    动态引入词库

--- src/view/Vocabulary.vue
+++ src/view/Vocabulary.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
-import cet4 from "../vocabulary/CET4.json"
+
+const VOCABULARY_IMPORTS = {
+    '初中': () => import('../vocabulary/json/1-初中-顺序.json'),
+    '高中': () => import('../vocabulary/json/2-高中-顺序.json'),
+    'CET4': () => import('../vocabulary/json/3-CET4-顺序.json'),
+    'CET6': () => import('../vocabulary/json/4-CET6-顺序.json'),
+    '考研': () => import('../vocabulary/json/5-考研-顺序.json'),
+    '托福': () => import('../vocabulary/json/6-托福-顺序.json'),
+    'SAT': () => import('../vocabulary/json/7-SAT-顺序.json'),
+}
+
+const KEY_LOCALSTORAGE_KEY = "known_words"
+
 import {computed, onMounted, ref, watch} from "vue";
 
-const wordsKnow = ref<String[]>([])
+let knownWordsSet = new Set()
+
+const currentVocabulary = ref<any[]>([])
 
 const wordsRandom = computed(() => {
-    return shuffle(cet4)
+    return shuffle(currentVocabulary.value)
 })
 
 /**
@@ -23,27 +37,33 @@ function shuffle(arr: Array<any>) {
     return arr;
 }
 
-onMounted(() => {
-    const storedValue = localStorage.getItem('wordsKnow')
+async function loadVocabulary(name: string) {
+    const module = await VOCABULARY_IMPORTS[name]()
+    currentVocabulary.value = module.default
+}
 
+onMounted(async () => {
+    await loadVocabulary('初中')
+    const storedValue = localStorage.getItem(KEY_LOCALSTORAGE_KEY)
     if (storedValue) {
         try {
-            wordsKnow.value = JSON.parse(storedValue)
+            knownWordsSet = new Set(JSON.parse(storedValue))
         } catch {
-            wordsKnow.value = []
+            knownWordsSet = new Set()
         }
     }
 })
 
-watch(wordsKnow, () => {
-    localStorage.setItem("wordsKnow", JSON.stringify(wordsKnow.value));
+watch(() => knownWordsSet, () => {
+    localStorage.setItem(KEY_LOCALSTORAGE_KEY, JSON.stringify(Array.from(knownWordsSet)));
 }, {deep: true})
+
 </script>
 
 <template>
     <div class="word-list">
-        <div :class="['word', {known: wordsKnow.includes(item.word)}]"
-             @click="wordsKnow.push(item.word)"
+        <div :class="['word', {known: knownWordsSet.has(item.word)}]"
+             @click="knownWordsSet.add(item.word)"
              v-for="item in wordsRandom"
              :key="item"
         >{{ item.word }}
